640d522cb6d36b1edc1777455b50bdf2e49dca4d
Csaba94 <33690319+Csaba94@users.noreply.github.com>
2018-01-03 13:56:52 +0200

    Raspberry pi setup file

---
+++ Raspberry_Web_page/setup.sh
@@ -0,0 +1,15 @@
+apt-get update
+apt-get upgrade
+apt-get install python3-pip
+apt-get install python-pip
+apt-get install nginx
+pip install pi-ina219
+apt-get install git-core
+cd
+git clone git://git.drogon.net/wiringPi
+cd ~/wiringPi
+git pull origin
+cd ~/wiringPi
+./build
+cd
+apt-get install php-fpm
